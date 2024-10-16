bdf24bbcccf904ec30ed7ffebdeb968e5306ebce
Nikitha <nikitha.medikonda@everest.engineering>
2024-10-16 10:48:12 +0530

    Add test for dashboard

---
+++ src/tests/dashboard.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import Dashboard from '../Pages/dashboard';
+import { UserContext } from '../context/context';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Dashboard Component', () => {
+    const mockUser = { username: 'testUser' };
+
+    const renderComponent = (user: any) => {
+        render(
+            <UserContext.Provider
+                value={{
+                    user,
+                    setUser: jest.fn(),
+                    transactions: [],
+                    setTransactions: jest.fn(),
+                    add: false,
+                    setAdd: jest.fn()
+                }}>
+                <MemoryRouter>
+                    <Dashboard />
+                </MemoryRouter>
+            </UserContext.Provider>
+        );
+    };
+
+    test('renders the dashboard header correctly', () => {
+        renderComponent(mockUser);
+        expect(screen.getByText('Fin')).toBeInTheDocument();
+        expect(screen.getByText('Grow')).toBeInTheDocument();
+    });
+});
